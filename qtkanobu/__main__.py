e4b9eb465c024bcaf3f652ffd0408507d1e2a0dd
jDan735 <daniel734@bk.ru>
2020-07-14 12:12:40 +0000

    v0.0.2

--- qtkanobu/__main__.py
+++ qtkanobu/__main__.py
@@ -1,2 +1,17 @@
+import sys
+from PyQt5 import QtWidgets
+from qtkanobu.mainwindow import design
+    
+class QtKanobu(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super(QtKanobu, self).__init__()
+        self.setupUi(self)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtKanobu()
+    window.show()
+    app.exec_()
+    
 if __file__ == "__main__":
-    pass
+    main()
--- qtkanobu/kanobu.py
+++ qtkanobu/kanobu.py
@@ -4,8 +4,6 @@ def main():
 
     while True:
 
-        print(locale["bot"]["choice"] + ".", end="")
-
         bot = random.randint(0, 2)
 
         massive = [
@@ -16,21 +14,10 @@ def main():
 
         i = 0
         for key in massive[player]:
-            a = "" if locale["lang"]["case"] is False else locale["lang"]["case"][bot]
-
             object = locale["objects"][bot]
             object = object if locale["lang"]["case"] is False else object.lower()
 
             if bot == i:
-
-                if i == 0:
-                    message = yellow(" " + locale["results"][key] + "!")
-
-                if i == 1:
-                    message = green(" " + locale["results"][key] + "!")
-
-                if i == 2:
-                    message = red(" " + locale["results"][key] + "!")
 
                 print(message + " " + locale["bot"]["have"] + a + " " + object)
 
